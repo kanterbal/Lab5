bb8c89037c89e6bd1eb714c2b967411c494035d0
Sergei Kanter <sdkanter@outlook.com>
2021-10-06 16:22:03 +0200

    ex1 done.

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -10,6 +10,20 @@ namespace Ex1
     {
         static void Main(string[] args)
         {
+            const int n = 7;
+            float s = 0;
+            int[] array = new int[n];
+            Console.WriteLine("Введите {0} чисел массива", n);
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            foreach (int a in array)
+            {
+                s += a;
+            }
+            Console.WriteLine("Среднее арифметическое элементов = {0:f2}", s / n);
+            Console.ReadKey();
         }
     }
 }
